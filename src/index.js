ea2368758659efc780786da23e0534089b86d31c
MACorman <madelinecorman@gmail.com>
2020-01-17 10:54:43 -0500

    comment form, create comment in database, set username, welcome page

--- src/index.js
+++ src/index.js
@@ -5,9 +5,41 @@ document.addEventListener('DOMContentLoaded', function(){
 
     let mainHeader = document.getElementById('header-container')
     let mainBody = document.getElementById('body-container')
+    let username
+    let langDiv = document.createElement("div")
+    langDiv.id = "language-container"
+    let welcomeDiv = document.createElement("div")
+    welcomeDiv.innerHTML = `
+        <h1>CodeHub</h1>
+        <h3>Welcome to CodeHub</h3>
+        <h4>A programming knowledge destination</h4>
+        <h4>Your one stop shop for all your programming needs</h4>
+        <h4>15 minutes could teach you 15% or more</h4>
+    `
+
+    
     
 
     getLanguage()
+    
+    function usernameForm() {
+        let userForm = document.createElement("form")
+        userForm.innerHTML = `
+        <input type="text" name="username" placeholder="Username">
+        <input type="submit" value="Enter Website"> 
+        `
+        mainHeader.appendChild(userForm)
+
+        userForm.addEventListener("submit", (e) => {
+            e.preventDefault()
+            username = e.target.username.value
+            // debugger
+            mainHeader.replaceChild(langDiv, userForm)
+            mainBody.replaceChild(welcomeDiv, mainBody.firstChild)
+        })
+    }
+    usernameForm()
+    // debugger
 
     function getLanguage(){
         fetch(languagesUrl).then(resp => resp.json())
@@ -20,15 +52,12 @@ document.addEventListener('DOMContentLoaded', function(){
         langSpan.id = language.name
         langSpan.dataset.id = language.id
         langSpan.innerText = `     ${language.name}     `
-        mainHeader.appendChild(langSpan)
+        langDiv.appendChild(langSpan)
 
         langSpan.addEventListener('click', function(e){
             let videoUl = document.createElement('ul')
             mainBody.replaceChild(videoUl, mainBody.firstChild)
             let id = parseInt(e.target.dataset.id)
-            // while (videoUl.hasChildNodes()) {  
-            //     videoUl.removeChild(videoUl.firstChild);
-            // }
             getVideos(id, videoUl)
               
 
@@ -39,7 +68,6 @@ document.addEventListener('DOMContentLoaded', function(){
     function getVideos(id, videoUl) {
         fetch(videosUrl).then(resp => resp.json())
         .then(result => result.map(video => {
-            // console.log(video)
             if (video.language.id === id) {
                 listVideos(video, videoUl)
             }
@@ -65,6 +93,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
         titleH3.addEventListener('click', function(e){
             let videoDiv = document.createElement('div')
+            videoDiv.dataset.id = e.target.dataset.id
             mainBody.replaceChild(videoDiv, mainBody.firstChild)
             let vidId = e.target.dataset.id
             getSingleVideo(vidId, videoDiv)
@@ -86,9 +115,7 @@ document.addEventListener('DOMContentLoaded', function(){
         let commUl = document.createElement('ul')
 
         video.comments.forEach(comment => {
-            let commentLi = document.createElement('li')
-            commentLi.innerText = `${comment.username}: ${comment.content}`
-            commUl.appendChild(commentLi)
+            renderComment(comment, commUl)
         })
 
         titleH2.innerText = video.title
@@ -104,14 +131,52 @@ document.addEventListener('DOMContentLoaded', function(){
         videoDiv.appendChild(videoI)
         videoDiv.appendChild(pubH3)
         videoDiv.appendChild(descP)
+        videoDiv.appendChild(commentForm(commUl))
         videoDiv.appendChild(commUl)
 
     }
 
+    function renderComment(comment, commUl) {
+        let commentLi = document.createElement('li')
+            commentLi.innerText = `${comment.username}: ${comment.content}`
+            commUl.appendChild(commentLi)
+    }
+
+    function commentForm(commUl) {
+        let commForm = document.createElement("form")
+        commForm.innerHTML = `
+            <input type="text" name="content" placeholder="Add a Comment">
+            <input type="submit">
+        `
+        commForm.addEventListener("submit", (e) => {
+            e.preventDefault()
+            let videoId = e.target.parentNode.dataset.id
+            let commContent = e.target.content.value
+            postComment(username, commContent, videoId, commUl)
+            e.target.reset()
+        })
+
+        return commForm
+    }
+
+    function postComment(username, commContent, id, commUl) {
+        fetch(commentsUrl, {
+            method: "POST", 
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }, 
+            body: JSON.stringify({username: username, content: commContent, video_id: id})
+        })
+        .then(resp => resp.json())
+        .then(comment => {
+            renderComment(comment, commUl)
+
+        })
+    }
 
 
-
-
+    
 
 
 
