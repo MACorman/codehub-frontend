fdac36b3f7dd1e94c8150c58be73a47ce6bc0d34
MACorman <madelinecorman@gmail.com>
2020-01-21 14:41:01 -0500

    delete comments function, submit video form and function, css

--- src/index.js
+++ src/index.js
@@ -41,6 +41,8 @@ document.addEventListener('DOMContentLoaded', function(){
             mainHeader.innerHTML = `
             <img id='background' src="src/backg2.jpeg" alt="">
             <img id='logo' height="13%" width="13%" src="src/logo-1.png" alt="">
+            <span id='submit-video'>Submit Video</span>
+            <span id='break'> | </span>
             <span id='logout'>Logout</span>
             <hr>
             `
@@ -164,13 +166,32 @@ document.addEventListener('DOMContentLoaded', function(){
 
     function renderComment(comment, commUl) {
         let commentLi = document.createElement('li')
+            commentLi.dataset.id = comment.id
             commentLi.innerHTML = `
             <h3 class='comment-author'>${comment.username}</h3> 
-            <p class='comment-content'>${comment.content}</p>
-            <hr>
+            <span class='comment-content'>${comment.content}</span>
             `
             commentLi.className = 'comments'
             commUl.appendChild(commentLi)
+
+            if (username === comment.username) {
+               let deleteBtn = document.createElement("button")
+               deleteBtn.className = "delete-btn"
+               deleteBtn.innerText = "Delete"
+               commentLi.appendChild(deleteBtn)
+               
+            }
+            let hr = document.createElement("hr")
+            hr.className = "comment-hr"
+            commentLi.appendChild(hr)
+            
+
+            commentLi.addEventListener("click", (e) => {
+                if (e.target.className === "delete-btn") {
+                    let commentId = parseInt(e.target.parentNode.dataset.id)
+                    deleteComment(commentLi, commentId)
+                }
+            })
     }
 
     function commentForm(commUl) {
@@ -209,8 +230,115 @@ document.addEventListener('DOMContentLoaded', function(){
     mainHeader.addEventListener("click", (e) => {
         if (e.target.id === "logout") {
             username = window.prompt("Enter Username")
+            mainBody.replaceChild(welcomeDiv, mainBody.firstChild)
+        }
+        else if (e.target.id === "submit-video") {
+            submitVidForm()
         }
     })
+
+    function deleteComment(commentLi, commentId) {
+        fetch(`${commentsUrl}/${commentId}`, {
+            method: "DELETE"
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            commentLi.remove() 
+        })
+    }
+
+    function submitVidForm() {
+
+        let formContainer = document.createElement("div")
+        
+        let submitMessage = document.createElement("h2")
+        submitMessage.className = "new-video-header"
+        submitMessage.innerText = "Submit New Video"
+
+        let videoForm = document.createElement("form")
+
+        let titleInput = document.createElement("input")
+        titleInput.className = "video-form"
+        titleInput.type = "text"
+        titleInput.placeholder = "Title"
+        titleInput.name = "title"
+
+
+        let pubInput = document.createElement("input")
+        pubInput.className = "video-form"
+        pubInput.type = "text"
+        pubInput.placeholder = "Publisher"
+        pubInput.name = "publisher"
+
+        let descInput = document.createElement("input")
+        descInput.className = "video-form"
+        descInput.type = "text"
+        descInput.placeholder = "Description"
+        descInput.name = "description"
+
+        let keyInput = document.createElement("input")
+        keyInput.className = "video-form"
+        keyInput.type = "text"
+        keyInput.placeholder = "Key"
+        keyInput.name = "key"
+        
+        let langSelect = document.createElement("select")
+        langSelect.className = "video-form"
+        langSelect.name = "language"
+
+        fetch(languagesUrl).then(resp => resp.json()).then(languages => {
+            languages.map(language => {
+                let langOption = document.createElement("option")
+                langOption.value = language.id
+                langOption.innerText = language.name
+                langSelect.append(langOption)
+            })
+        })
+
+        videoForm.append(titleInput)
+        videoForm.append(pubInput)
+        videoForm.append(descInput)
+        videoForm.append(keyInput)
+        videoForm.append(langSelect)
+        
+        let videoSubmit = document.createElement("input")
+        videoSubmit.id = "video-submit-btn"
+        videoSubmit.type = "submit"
+        videoForm.append(videoSubmit)
+        formContainer.append(submitMessage)  
+        formContainer.append(videoForm)
+        
+        mainBody.replaceChild(formContainer, mainBody.firstChild)
+
+        videoForm.addEventListener("submit", (e) => {
+            e.preventDefault()
+            let title = e.target.title.value
+            let publisher = e.target.publisher.value
+            let desc = e.target.description.value
+            let key = e.target.key.value
+            let language = parseInt(e.target.language.value)
+
+            let newVideo = {title: title, publisher: publisher, description: desc, key: key, language_id: language}
+
+            fetch(videosUrl, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                }, 
+                body: JSON.stringify(newVideo)
+            })
+            .then(resp => resp.json())
+            .then(data => {
+                alert("Video has been submitted.")
+                mainBody.replaceChild(welcomeDiv, mainBody.firstChild)
+            })
+        })
+    }
+
+    
+    
+
    
 
     
